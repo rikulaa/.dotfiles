c03376ad70a2d2bea041c41b1796beb97d4b1feb
RikLaa <riku.laajala@gmail.com>
2017-03-22 00:34:13 +0200

    added bashrc to install.sh as well

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim zshrc gitconfig aliases"    # list of files/folders to symlink in homedir
+files="vimrc vim zshrc bashrc gitconfig aliases"    # list of files/folders to symlink in homedir
 
 ##########
 
