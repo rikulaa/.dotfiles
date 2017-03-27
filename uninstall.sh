55dcb38c3f38ec52ece4b26d287a352da6bf04a9
RikLaa <riku.laajala@gmail.com>
2017-03-27 21:28:12 +0300

    added uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash                                                                                                                                                                                                                                
+#remove everything
+rm ~/.aliases
+rm ~/.bashrc
+rm ~/.tmux.conf
+rm ~/.vimrc
+rm ~/.zshrc
+rm ~/.gitconfig
+rm -Rf ~/.vim
+rm -Rf ~/.oh-my-zsh
+rm -Rf ~/bin
