8cdbc62d632bcfb74c233bb2a6cb41ab7cd4c52b
Riku Laajala <riku.laajala@gmail.com>
2020-04-16 23:30:28 +0300

    Rename, git.sh => git.adoc

--- cheatsheets/git.sh
+++
@@ -1,8 +0,0 @@
-# Add second remote, pull from it
-
-git remote add something git://host.example.com/path/to/repo.git
-git checkout master
-git pull something master
-
-# Pull without adding remote
-git pull git://host.example.com/path/to/repo.git
