563142c281c4ef2e4e6e77291531c9a9448ae032
Riku Laajala <riku.laajala@gmail.com>
2022-02-12 01:17:09 +0200

    use nix-env -iA to avoid lazy evaluation
    Lazy evaluation consumes a lot of memory

--- .bin/nix-programs.sh
+++ .bin/nix-programs.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 # Install's the following programs AND deletes everything else NOT on this list
-nix-env -i -r \
-    stow \
-    direnv \
-    fzf \
-    jq \
-    ripgrep \
-    shellcheck \
-    asciidoctor \
-    tmux \
-    wget \
-    imagemagick \
-	htop \
-    ffmpeg 
+nix-env -iA \
+    nixpkgs.stow \
+    nixpkgs.direnv \
+    nixpkgs.fzf \
+    nixpkgs.jq \
+    nixpkgs.ripgrep \
+    nixpkgs.shellcheck \
+    nixpkgs.asciidoctor \
+    nixpkgs.tmux \
+    nixpkgs.wget \
+    nixpkgs.imagemagick \
+	nixpkgs.htop \
+    nixpkgs.ffmpeg 
 
 # neovim 6.0.0
 nix-env -i neovim-0.6.0 -f https://github.com/NixOS/nixpkgs/archive/89f196fe781c53cb50fef61d3063fa5e8d61b6e5.tar.gz
